55c2d5ea0d8b2e78047bcb62e6df79f54cef123b
MAC <mac@MACs-MacBook-Pro.local>
2023-10-10 20:04:43 +0200

    modified jenkins

--- terraform-script/module/jenkins/jenkins_script.tf
+++ terraform-script/module/jenkins/jenkins_script.tf
@@ -11,7 +11,17 @@ sudo rpm -ivh jenkins-2.411-1.1.noarch.rpm
 sudo yum update -y
 sudo yum install jenkins -y
 sudo systemctl start jenkins
-sudo systemctl enable jenkins 
+sudo systemctl enable jenkins
+#install docker
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+sudo usermod -aG docker jenkins
+sudo chmod 777 /var/run/docker.sock
+sudo service sshd restart
 sudo hostnamectl set-hostname Jenkins
 EOF
 }
